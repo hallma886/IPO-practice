7aa0e98c6b3c1ccb5fd89ffa1797a9af0eb13525
hallma886 <hallma886@northwested.org>
2024-09-18 14:25:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Matthew Hall
+# September 18, 2024
+# IPO Intro
+
+num1 = float(input('enter a number: \n'))
+num2 = float(input('enter another number: \n'))
+
+total = num1 * num2
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The product of {num1} and {num2} is: {total}')
+
+
+
+ 
+
+
+
+
+
